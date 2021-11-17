2e130a8f9c059079c870aa4b62d0e4fe89d2886f
Stanley Ho <stanley_ho@apple.com>
2021-11-17 01:13:23 -0800

    Changed date format in tweetscollect.

--- cmd/tweetscollect/date.go
+++ cmd/tweetscollect/date.go
@@ -3,6 +3,10 @@ package main
 import "time"
 
 // parseDate parses date and return time.Time. e.g. "Fri Nov 12 20:38:53 +0000 2021".
-func parseDate(d string) (time.Time, error) {
-	return time.Parse(time.RubyDate, d)
+func parseDate(d string) (string, error) {
+	t, err := time.Parse(time.RubyDate, d)
+	if err != nil {
+		return "", err
+	}
+	return t.Format(time.RFC3339), nil
 }
